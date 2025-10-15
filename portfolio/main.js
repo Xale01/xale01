ef8b7bd8f910fa964843373ded1315997dbcb1bd
AxelAvecDuSel <107510348+Xale01@users.noreply.github.com>
2025-10-15 11:21:47 +0200

    Delete portfolio directory

--- portfolio/main.js
+++
@@ -1,6 +0,0 @@
-const div = document.querySelector("div")
-div.addEventListener("click", _ =>{
-    const newP = document.createElement("p")
-    newP.innerText = "OULALA"
-    div.appendChild(newP)
-})
