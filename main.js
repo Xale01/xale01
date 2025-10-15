ad452c42dbe5c60550cc23338858e5c5072428d7
Axel Benard <Axel.Benard@etu.univ-smb.fr>
2025-10-15 20:40:57 +0200

    bloqué la liste pour pas dépasser 6

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const div = document.querySelector("div")
 let count = 0
 const liste = ["pop", "holala", "hola?", "hola.", "eh j'ai dit hola", "eh faut répondre", "RÉPOND WESH"]
 div.addEventListener("click", _ =>{
-    const newP = document.createElement("p")
-    newP.innerText = liste[count]
-    count += 1
-    div.appendChild(newP)
+    if (count <= 6){
+        const newP = document.createElement("p")
+        newP.innerText = liste[count]
+        count += 1
+        div.appendChild(newP)
+    }
 })
