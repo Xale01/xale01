5e87dcae0959c55207f503a0a5955125d82f6a66
AxelAvecDuSel <107510348+Xale01@users.noreply.github.com>
2025-10-15 11:26:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const div = document.querySelector("div")
+let count = 0
+const liste = ["pop", "holala", "hola?", "hola.", "eh j'ai dit hola", "eh faut répondre", "RÉPOND WESH"]
 div.addEventListener("click", _ =>{
     const newP = document.createElement("p")
-    newP.innerText = "OULALA"
+    newP.innerText = liste[count]
+    count += 1
     div.appendChild(newP)
 })
